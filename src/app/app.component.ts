4026e5fb1c693b625fb7ac1fb4e8e1f78f357c40
Curt Grogan <curt.grogan@fdmgroup.com>
2023-10-07 13:21:55 -0400

    Added course-card component and functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import {COURSES} from '../db-data';
+import { COURSES } from 'src/db-data';
+import { Course } from './model/course';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,11 @@ import {COURSES} from '../db-data';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+    coreCourse: Course = COURSES[0];
+    rxjsCourse = COURSES[1]; // Can infer type.
+    ngrxCourse: Course = COURSES[2];
+
 
 
 
